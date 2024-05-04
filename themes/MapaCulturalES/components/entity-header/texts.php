38068b44b9255d6494c4f07321dc93726918171b
PhenBD <pedro.h.duarte@edu.ufes.br>
2024-05-04 19:39:43 -0300

    correção da exibição de taxonomias

---
+++ themes/MapaCulturalES/components/entity-header/texts.php
@@ -0,0 +1,12 @@
+<?php
+use MapasCulturais\i;
+
+return [
+    'title agent-1' => i::__('Edição da pessoa física'),
+    'title agent-2' => i::__('Edição da pessoa jurídica'),
+    'title project' => i::__('Edição do projeto'),
+    'title space' => i::__('Edição do espaço'),
+    'title opportunity' => i::__('Edição da oportunidade'),
+    'title event' => i::__('Edição do evento'),
+    'title seal' => i::__('Edição do selo')
+];
