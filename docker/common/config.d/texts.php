2708835b3beec173f53caacc09580e30a813c756
Propato <david123propato@gmail.com>
2024-06-01 00:49:25 -0300

    Updates: Images & Developer Text

--- docker/common/config.d/texts.php
+++ docker/common/config.d/texts.php
@@ -6,17 +6,20 @@ return [
     "text:home-header.description" => i::__("O Mapa de Inovação do Espírito Santo é uma plataforma livre, colaborativa e interativa que mapeia e promove o cenário de inovação no estado. Além de ser um instrumento de governança digital, o mapa contribui para o aprimoramento da gestão pública, dos mecanismos de participação e da democratização do acesso às políticas de inovação promovidas no Espírito Santo. Por meio dele, é possível estruturar sistemas de informações e indicadores que impulsionam o desenvolvimento e a promoção da inovação no estado."),
 
     /* Esses textos estão sendo modificados diretamente no arquivo dentro do tema */
-    // "text:home-entities.title" => i::__('Aqui você encontra as informações da inovação em sua região!'),
-    // "text:home-entities.description" => i::__('Mas para isso, precisamos da sua ajuda! Faça você também: cadastre suas iniciativas, empresas e eventos.'),
-    // "text:home-entities.opportunities" => i::__('É aqui que você pode realizar inscrições e acessar o resultado de editais e chamamentos públicos de inovação. Você também pode criar o seu próprio formulário e divulgar sua oportunidade para outros agentes da inovação.'),
-    // "text:home-entities.events" => i::__('Quer saber quais os eventos de inovação que estão ocorrendo em sua região? Basta fazer uma pesquisa a partir das ferramentas de busca. Além disso, como usuário cadastrado, você pode incluir seus eventos na plataforma e divulgá-los gratuitamente!'),
-    // "text:home-entities.spaces" => i::__('Você pode procurar por espaços de inovação incluídos na plataforma a partir dos campos de busca combinada que ajudam na precisão de sua pesquisa. Cadastre os espaços onde desenvolve suas atividades de inovação e empreendedorismo.'),
-    // "text:home-entities.agents" => i::__('Aqui você pode encontrar todos os profissionais, empreendedores e entidades que estão registrados na plataforma. É uma rede de agentes que estão envolvidos na cena de inovação da região. Você também pode realizar o seu cadastro, bem como o de seus coletivos, equipes, instituições e empresas das quais faça parte.'),
-    // "text:home-entities.projects" => i::__('Este é o local onde você pode encontrar oportunidades de financiamento, eventos, convocatórias e editais de inovação criados, além de diversas iniciativas cadastradas pelos usuários da plataforma.'),
+    // "text:home-entities.title" => i::__("Aqui você encontra as informações da inovação em sua região!"),
+    // "text:home-entities.description" => i::__("Mas para isso, precisamos da sua ajuda! Faça você também: cadastre suas iniciativas, empresas e eventos."),
+    // "text:home-entities.opportunities" => i::__("É aqui que você pode realizar inscrições e acessar o resultado de editais e chamamentos públicos de inovação. Você também pode criar o seu próprio formulário e divulgar sua oportunidade para outros agentes da inovação."),
+    // "text:home-entities.events" => i::__("Quer saber quais os eventos de inovação que estão ocorrendo em sua região? Basta fazer uma pesquisa a partir das ferramentas de busca. Além disso, como usuário cadastrado, você pode incluir seus eventos na plataforma e divulgá-los gratuitamente!"),
+    // "text:home-entities.spaces" => i::__("Você pode procurar por espaços de inovação incluídos na plataforma a partir dos campos de busca combinada que ajudam na precisão de sua pesquisa. Cadastre os espaços onde desenvolve suas atividades de inovação e empreendedorismo."),
+    // "text:home-entities.agents" => i::__("Aqui você pode encontrar todos os profissionais, empreendedores e entidades que estão registrados na plataforma. É uma rede de agentes que estão envolvidos na cena de inovação da região. Você também pode realizar o seu cadastro, bem como o de seus coletivos, equipes, instituições e empresas das quais faça parte."),
+    // "text:home-entities.projects" => i::__("Este é o local onde você pode encontrar oportunidades de financiamento, eventos, convocatórias e editais de inovação criados, além de diversas iniciativas cadastradas pelos usuários da plataforma."),
 
-    "text:home-feature.description" => i::__('Confira as últimas novidades de cada uma das entidades de inovação'),
+    "text:home-feature.description" => i::__("Confira as últimas novidades de cada uma das entidades de inovação"),
 
-    "text:home-register.title" => i::__('Faça seu cadastro e colabore com o Mapa da Inovação'),
-    "text:home-register.description" => i::__('Colabore com a plataforma livre, colaborativa e interativa de mapeamento do cenário de inovação e instrumento de governança digital no aprimoramento da gestão pública, dos mecanismos de participação e da democratização do acesso às políticas de inovação promovidas pelo estado.
-    '),
+    "text:home-register.title" => i::__("Faça seu cadastro e colabore com o Mapa da Inovação"),
+    "text:home-register.description" => i::__("Colabore com a plataforma livre, colaborativa e interativa de mapeamento do cenário de inovação e instrumento de governança digital no aprimoramento da gestão pública, dos mecanismos de participação e da democratização do acesso às políticas de inovação promovidas pelo estado.
+    "),
+
+    "text:home-developers.title" => i::__("Alô desenvolvedores!"),
+    "text:home-developers.description" => i::__("Além disso, o Mapa de Inovação é um software livre, criado em parceria entre a hacklab/, secretarias de inovação, organizações não governamentais, empresas e coletivos que investem na plataforma. Você pode contribuir para o seu desenvolvimento através do GitHub.")
 ];
